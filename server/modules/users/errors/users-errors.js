693c5fd478ddceddeff6c59753d05e8777dc2030
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-03-04 06:54:55 +0200

    edit: create token related users errors

--- server/modules/users/errors/users-errors.js
+++ server/modules/users/errors/users-errors.js
@@ -14,7 +14,23 @@ class EmailAddressAlreadyInUseError extends Error {
   }
 }
 
+class NoTokenProvidedError extends Error {
+  constructor() {
+    super("Access denied, no token provided.");
+    this.statusCode = statusCode.error.client.unauthorized;
+  }
+}
+
+class InvalidTokenProvidedError extends Error {
+  constructor() {
+    super("Access denied, invalid token provided.");
+    this.statusCode = statusCode.error.client.bad_request;
+  }
+}
+
 module.exports = {
   InvalidCredentialsError,
   EmailAddressAlreadyInUseError,
+  NoTokenProvidedError,
+  InvalidTokenProvidedError,
 };
