b8e967774075e13e148f9b2beccf1212bd2a5051
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-03-11 10:17:25 +0200

    edit: shifts service

--- server/modules/shifts/business-logic/shifts-service.js
+++ server/modules/shifts/business-logic/shifts-service.js
@@ -1,22 +1,7 @@
-const { Shift } = require("../../../db/models");
 const shiftsDataAccess = require("../data-access/shifts-data-access");
-const profilesService = require("../../profiles/business-logic/profiles-service");
-
-const vat = 0.17;
 
 async function submitShift(values) {
-  const { uid, deliveries, creditTips } = values;
-  const profile = await profilesService.getProfileByUid(uid);
-  const fee = profile.monthlyEmployerCompanyFee;
-  const deliveriesFee = Shift.calculateFee(deliveries, fee);
-  const creditTipsVat = Shift.calculateVat(creditTips, vat);
-  const creditTipsFee = Shift.calculateFee(creditTips - creditTipsVat, fee);
-  return await shiftsDataAccess.submitShift({
-    deliveriesFee,
-    creditTipsVat,
-    creditTipsFee,
-    ...values,
-  });
+  return await shiftsDataAccess.submitShift(values);
 }
 
 async function getShiftsByUidAndMonth(uid, month) {
