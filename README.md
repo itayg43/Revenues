7eb410888268745367760c4355c6c45367092863
itayg43 <93944494+itayg43@users.noreply.github.com>
2024-02-29 02:15:48 +0200

    edit: add seed file with create user function

--- README.md
+++ README.md
@@ -12,6 +12,8 @@ cd server && npm i
 npx prisma migrate dev --name init
 ```
 
+When npx prisma migrate dev is executed against a newly created database, seeding is also triggered. The seed file in prisma/seed.ts will be executed and your database will be populated with the sample data.
+
 ```
 npm run start:dev
 ```
---
+++ server/prisma/seed.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client";
+
+const prismaClient = new PrismaClient();
+
+(async () => {
+  try {
+    await createUser();
+    await prismaClient.$disconnect();
+  } catch (error) {
+    console.error(error);
+    await prismaClient.$disconnect();
+    process.exit(1);
+  }
+})();
+
+async function createUser() {
+  console.log("Start seeding...");
+
+  const user = await prismaClient.user.create({
+    data: {
+      email: "itayg43@gmail.com",
+      firstName: "Itay",
+      lastName: "Gur",
+    },
+  });
+
+  console.log("Seeding finished");
+}
